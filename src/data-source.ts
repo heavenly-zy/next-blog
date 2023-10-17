ddcd379974d96f7e250959d91a700be1baeec64a
ClariS <1457715339@qq.com>
2023-10-17 23:43:04 +0800

    feat: 成功连接到数据库

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,16 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
-import { User } from "./entity/User"
 
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: "localhost",
   port: 5432,
-  username: "test",
-  password: "test",
-  database: "test",
+  username: "blog",
+  password: "",
+  database: "blog_development",
   synchronize: true,
   logging: false,
-  entities: [User],
+  entities: ["dist/entity/**/*.js"],
   migrations: [],
   subscribers: [],
 })

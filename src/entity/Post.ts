0e07e0c75a7b965cbb582280e6dc73c7bd58b27d
ClariS <1457715339@qq.com>
2023-10-20 15:54:05 +0800

    feat: 创建三个实体的数据关联

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,4 +1,14 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn
+} from "typeorm"
+import { User } from "./User"
+import { Comment } from "./Comment"
 
 @Entity("posts")
 export class Post {
@@ -8,8 +18,12 @@ export class Post {
   title: string
   @Column("text")
   content: string
-
-  constructor(attributes: Partial<Post>) {
-    Object.assign(this, attributes)
-  }
+  @CreateDateColumn()
+  createdAt: Date
+  @UpdateDateColumn()
+  updatedAt: Date
+  @ManyToOne((type) => User, (user) => user.posts)
+  author: User
+  @OneToMany((type) => Comment, (comment) => comment.post)
+  comments: Comment[]
 }
--- src/seed.ts
+++ src/seed.ts
@@ -1,19 +1,33 @@
 import { AppDataSource } from "./data-source"
+import { Comment } from "./entity/Comment"
 import { Post } from "./entity/Post"
+import { User } from "./entity/User"
 
 AppDataSource.initialize()
   .then(async (connection) => {
-    const posts = await connection.manager.find(Post)
-    if (posts.length === 0) {
-      await connection.manager.save(
-        Array.from(new Array(40), (_, index) => {
-          return new Post({ title: `Post ${index + 1}`, content: `这是我的第${index + 1}篇文章` })
-        })
-      )
-      console.log("posts 数据填充了")
-    }
-    if (connection.isInitialized) {
-      await connection.destroy()
-    }
+    // 创建 user1
+    const u1 = new User()
+    u1.username = "ClariS"
+    u1.passwordDigest = "123456"
+    await connection.manager.save(u1)
+    console.log("users 数据填充了")
+
+    // 创建 post1
+    const p1 = new Post()
+    p1.title = "Post 1"
+    p1.content = "My First Post"
+    p1.author = u1
+    await connection.manager.save(p1)
+    console.log("posts 数据填充了")
+
+    // 创建 comment1
+    const c1 = new Comment()
+    c1.user = u1
+    c1.post = p1
+    c1.content = "xxx"
+    await connection.manager.save(c1)
+    console.log("comments 数据填充了")
+
+    connection.isInitialized && await connection.destroy()
   })
   .catch((error) => console.log(error))

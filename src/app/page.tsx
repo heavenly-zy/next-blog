b5f1bb9ffe32c00d5d9daff7688a8852f2991a50
ClariS <1457715339@qq.com>
2023-10-30 15:02:01 +0800

    perf: 首页复用 post 列表

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,3 @@
-import { Post } from "@/entity/Post"
-import { getDatabaseConnection } from "@/lib/getDatabaseConnection"
-import Link from "next/link"
+import PostsIndex from "./posts/page"
 
-export default async function Home() {
-  const connection = await getDatabaseConnection()
-  const posts = await connection.manager.find(Post)
-  console.log("posts: ", posts)
-  return (
-    <div>
-      <h1>文章列表</h1>
-      {posts.map((post) => (
-        <Link key={post.id} href={`/posts/${post.id}`}>
-          {post.title}
-        </Link>
-      ))}
-    </div>
-  )
-}
+export default PostsIndex

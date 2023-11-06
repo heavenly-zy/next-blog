606a8f5bce86a66353ca6910b9474178d4280747
ClariS <1457715339@qq.com>
2023-11-06 16:17:05 +0800

    feat: 首页调整为文章列表入口

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
-import PostsIndex from "./posts/page"
+import { NextPage } from "next"
+import Link from "next/link"
 
-export default PostsIndex
+const Home: NextPage = () => {
+  return (
+    <>
+      <div>
+        <h1>ClariS 的个人博客</h1>
+        <p>Querying for a variable and seizing the time</p>
+        <p>
+          <Link href="/posts">文章列表</Link>
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default Home

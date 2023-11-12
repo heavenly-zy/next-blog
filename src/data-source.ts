35df950ac5aab38d4bef9997b78486b1e053387b
ClariS <1457715339@qq.com>
2023-11-12 17:18:36 +0800

    chore: 区分数据库环境

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,13 +1,15 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 
+const database = process.env.NODE_ENV === "production" ? "blog_production" : "blog_development"
+
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: "localhost",
   port: 5432,
   username: "blog",
   password: "",
-  database: "blog_development",
+  database,
   synchronize: false,
   logging: false,
   entities: ["dist/entity/**/*.js"],
